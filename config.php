2b06a639ab76b4a494eb0dfbb0a21fb9b48ef5f1
WowhooS <67091845+WowhooS@users.noreply.github.com>
2021-03-22 07:54:59 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//This is the config.php file
+
+// Specify your login credentials
+$username   = "ivote405";
+$apikey     = "8ce38c89fb7b6ab6698937c8b3c1ef39a5248a659e01bbe0bad4ac934840431f";
+
+?>
